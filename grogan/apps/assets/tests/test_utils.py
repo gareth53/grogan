cbf1c34eaec6b86ea8ea4b4d60415d78c1048739
Gareth Senior <gareth.senior@thisisglobal.com>
2015-08-21 17:19:26 +0100

    another test :)

--- grogan/apps/assets/tests/test_utils.py
+++ grogan/apps/assets/tests/test_utils.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
 from grogan.apps.assets.utils import order_crops_by_suitability, get_crop_props
 
-def crop_factory(w, h, l, t, zoom_w, zoom_h, expected_order=None):
+def crop_factory(w, h, l, t, resize_width, resize_height, expected_order=None):
 	return {
 		'width': w,
 		'height': h,
 		'ratio': w/h,
 		'crop_left': l,
 		'crop_top': t,
-		'resize_width': zoom_w,
-		'resize_height': zoom_h,
+		'resize_width': resize_width,
+		'resize_height': resize_height,
 		'expected_order': expected_order
 	}
 
@@ -97,14 +97,37 @@ class GetCropProps(TestCase):
 		assert returned['crop_left'] == 0
 
 
-#	def test_get_crop_props_reworks_diff_aspect_ratio(self):
-#		crops = [
-#			crop_factory(1600, 600, 0, 100, 1600, 800),
-#		]
-#		returned = get_crop_props(1600, 800, crops, 800, 400)
-#		assert returned['resize_width'] == 1600
-#		assert returned['resize_height'] == 800
-#		assert returned['crop_top'] == 0
-#		assert returned['crop_right'] == 800
-#		assert returned['crop_bottom'] == 400
-#		assert returned['crop_left'] == 0
+	def test_get_crop_props_reworks_diff_aspect_ratio1(self):
+		"""
+		the crop here is shallower
+		"""
+		crops = [
+			crop_factory(1600, 600, 0, 100, 1600, 800),
+		]
+		returned = get_crop_props(1600, 800, crops, 800, 400)
+		assert returned['resize_width'] == 800
+		assert returned['resize_height'] == 400
+		assert returned['crop_top'] == 0
+		assert returned['crop_right'] == 800
+		assert returned['crop_bottom'] == 400
+		assert returned['crop_left'] == 0
+
+	def test_get_crop_props_reworks_diff_aspect_ratio2(self):
+		"""
+		the crop here is deeper
+		"""
+		crops = [
+			# (width, height, left, top, resize_width, resize_height
+			crop_factory(600, 500, 100, 100, 800, 400),
+		]
+		returned = get_crop_props(1600, 800, crops, 600, 400)
+		assert returned['resize_width'] == 800
+		assert returned['resize_height'] == 400
+		assert returned['crop_top'] == 0
+		assert returned['crop_bottom'] == 400
+		assert returned['crop_left'] == 100
+		assert returned['crop_right'] == 700
+
+# TODO, test rework of leak to the left
+# TODO test rework of height leak
+
