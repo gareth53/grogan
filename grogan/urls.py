8921e0324619cf6c7ae10ed11494a15d3bafb273
Gareth Senior <gareth.senior@thisisglobal.com>
2015-10-30 15:44:15 +0000

    removed dupe line

--- grogan/urls.py
+++ grogan/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/assets/', include('grogan.apps.assets.admin_urls')),
     url(r'^api/1.0/assets/', include('grogan.apps.assets.service_urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
